ca93317defa72fc45243cc91f502ed8f1f1e2b22
Kongtap (Arm) Arunlakvilart <kongtap@fintech.co.th>
2023-01-10 13:52:49 +0700

    refactor: test executing

--- tests/unit_test/test_executing.py
+++ tests/unit_test/test_executing.py
@@ -11,32 +11,15 @@ from ezyquant_execute.executing import execute
 class TestExecuting:
     @pytest.mark.parametrize("signal_dict", [{}, {"a": 1}, {"a": 1, "b": 2}])
     def test_signal_dict(self, signal_dict: Dict[str, Any]):
-        # Mock
-        m = Mock()
-
-        # Test
-        self._test(signal_dict=signal_dict, execute_algorithm=m)
-
-        # Check
-        for k, v in signal_dict.items():
-            m.assert_any_call(
-                ExecuteContext(settrade_user=ANY, event=ANY, symbol=k, signal=v)
-            )
+        self._test(signal_dict=signal_dict)
 
     def test_execute_algorithm_event_set(self):
         """If event.set() is called, this function will be stopped after iteration."""
         # Mock
         signal_dict = {"a": 1, "b": 2}
-        m = Mock(lambda x: x.event.set())
 
         # Test
-        self._test(signal_dict=signal_dict, execute_algorithm=m)
-
-        # Check
-        for k, v in signal_dict.items():
-            m.assert_any_call(
-                ExecuteContext(settrade_user=ANY, event=ANY, symbol=k, signal=v)
-            )
+        self._test(signal_dict=signal_dict)
 
     def test_execute_algorithm_raise(self):
         """If execute_algorithm raise exception, this function will be stopped immediately."""
@@ -54,13 +37,16 @@ class TestExecuting:
     def _test(
         self,
         signal_dict: Dict[str, Any] = {"a": 1},
-        execute_algorithm: Callable[[ExecuteContext], None] = Mock(),
+        execute_algorithm: Optional[Callable[[ExecuteContext], None]] = None,
         interval: float = 1.0,
         start_time: time = time(0, 0, 0),
         end_time: Optional[time] = None,
     ):
         if not end_time:
             end_time = (datetime.now() + timedelta(seconds=1)).time()
+
+        if not execute_algorithm:
+            execute_algorithm = Mock()
 
         execute(
             settrade_user=ANY,
@@ -70,3 +56,11 @@ class TestExecuting:
             start_time=start_time,
             end_time=end_time,
         )
+
+        if isinstance(execute_algorithm, Mock):
+            if not signal_dict:
+                execute_algorithm.assert_not_called()
+            for k, v in signal_dict.items():
+                execute_algorithm.assert_any_call(
+                    ExecuteContext(settrade_user=ANY, event=ANY, symbol=k, signal=v)
+                )
