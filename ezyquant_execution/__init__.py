2ce9971b7c66eb7c77fbe527b74e794a3883a948
Kongtap (Arm) Arunlakvilart <kongtap@fintech.co.th>
2023-01-11 18:28:38 +0700

    feat: setup

--- ezyquant_execution/__init__.py
+++ ezyquant_execution/__init__.py
@@ -1,1 +1,2 @@
+from ._version import __version__
 from .executing import execute_on_timer
--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ import re
 
 from setuptools import find_packages, setup
 
-NAME = "ezyquant"
+NAME = "ezyquant-execution"
 
-VERSIONFILE = "ezyquant/_version.py"
+VERSIONFILE = "ezyquant_execution/_version.py"
 verstrline = open(VERSIONFILE, "rt").read()
 VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
 mo = re.search(VSRE, verstrline, re.M)
@@ -15,34 +15,24 @@ else:
 
 setup(
     name=NAME,
-    packages=find_packages(include=["ezyquant", "ezyquant.*"]),
+    packages=find_packages(include=["ezyquant_execution", "ezyquant_execution.*"]),
     version=verstr,
-    description="Powerful backtest python library for Thai stocks",
-    long_description="Powerful backtest python library for Thai stocks",
     author="Fintech (Thailand) Company Limited",
     author_email="admin@fintech.co.th",
-    url="https://doc.ezyquant.com/",
+    # url="https://doc.ezyquant-execution.com/",
     maintainer="Fintech (Thailand) Company Limited",
     maintainer_email="admin@fintech.co.th",
-    python_requires=">=3.8.6",
-    install_requires=[
-        "pandas>=1.3",
-        "sqlalchemy>=1.4",
-        "psycopg2-binary>=2.9",
-        "ta>=0.10",
-        "XlsxWriter>=3.0",
-        "typing_extensions>=4.4",
-    ],
+    python_requires=">=3.8",
+    install_requires=["pandas>=1.3", "settrade-v2==2.1.2"],
     license="The MIT License (MIT)",
     classifiers=[
         "Intended Audience :: Financial and Insurance Industry",
-        "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
     project_urls={
-        "Documentation": "https://doc.ezyquant.com/",
-        "Bug Reports": "https://github.com/ezyquant/ezyquant/issues",
-        "Source": "https://github.com/ezyquant/ezyquant",
+        # "Documentation": "https://doc.ezyquant-execution.com/",
+        "Bug Reports": "https://github.com/ezyquant/ezyquant-execution/issues",
+        "Source": "https://github.com/ezyquant/ezyquant-execution",
     },
 )
