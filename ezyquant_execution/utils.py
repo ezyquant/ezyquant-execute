7d429702856ca5f3f41a1a0d71641417fe7b2372
Kongtap (Arm) Arunlakvilart <kongtap@fintech.co.th>
2023-01-13 18:33:22 +0700

    feat: match_tick_price

--- ezyquant_execution/utils.py
+++ ezyquant_execution/utils.py
@@ -1,5 +1,9 @@
+import math
 import time as t
 from datetime import datetime, time
+from functools import lru_cache
+
+import numpy as np
 
 """
 Time
@@ -39,27 +43,127 @@ Round
 """
 
 
-def round_volume_100(volume: float, round_mode: str = "down") -> int:
-    # round down
-    if round_mode.lower() == "down":
-        return round_down_100(volume)
-    # round up
-    elif round_mode.lower() == "up":
-        return round_up_100(volume)
-    # half up
+def round_100(value: float, is_round_up: bool = True) -> int:
+    """Round float to nearest 100.
+
+    Parameters
+    ----------
+    value : float
+        value to round
+    is_round_up : bool
+        is round up if value is not in 100, else round down.
+
+    Returns
+    -------
+    int
+        value after rounding
+    """
+    if value % 100 == 0:
+        return int(value)
+    if is_round_up:
+        return int(round((value + 50) / 100.0) * 100)
     else:
-        if volume % 100 >= 50:
-            return round_up_100(volume)
-        else:
-            return round_down_100(volume)
+        return int(value // 100) * 100
 
 
-def round_down_100(f: float) -> int:
-    return int((f + 1e-8) // 100 * 100)
+def round_even(value: float, is_round_up: bool = True) -> int:
+    """Round float to nearest even.
 
+    Parameters
+    ----------
+    value : float
+        value to round
+    is_round_up : bool
+        is round up if value is not in 100, else round down.
 
-def round_up_100(f: float) -> int:
-    if f % 100 == 0:
-        return int(f)
+    Returns
+    -------
+    int
+        value after rounding
+    """
+    if value % 2 == 0:
+        return int(value)
+    if is_round_up:
+        return int(round((value + 1) / 2.0) * 2)
     else:
-        return int(round_down_100(f) + 100)
+        return int(value // 2) * 2
+
+
+"""
+Price
+"""
+
+
+@lru_cache(maxsize=1)
+def _price_array() -> np.ndarray:
+    ranges = [
+        (0.01, 2, 0.01),
+        (2, 5, 0.02),
+        (5, 10, 0.05),
+        (10, 25, 0.1),
+        (25, 100, 0.25),
+        (100, 200, 0.5),
+        (200, 400, 1),
+        (400, 800, 2),
+    ]
+
+    prices = np.concatenate(
+        [np.arange(start, stop, step) for start, stop, step in ranges]
+    )
+    return np.round(prices, 2)
+
+
+@lru_cache
+def match_tick_price(price: float, n_tick: int, is_round_up: bool = True) -> float:
+    """Match price to tick price.
+
+    Parameters
+    ----------
+    price : float
+        price to match
+    n_tick : int
+        number of tick to move. positive for higher price, negative for lower price. maximum is 150
+    is_round_up : bool
+        is round up if price is not in tick price, else round down.
+
+    Returns
+    -------
+    float
+        price after matching tick price
+    """
+    if math.isnan(price):
+        return price
+
+    assert price > 0, "price should be greater than 0"
+
+    if price > 750:
+        price = round_even(price, is_round_up)
+        return float(price + 2 * n_tick)
+
+    idx = (
+        np.searchsorted(_price_array(), price, side="left" if is_round_up else "right")
+        + n_tick
+    )
+    if not is_round_up:
+        idx -= 1
+
+    if idx < 0:
+        idx = 0
+
+    return _price_array()[idx]
+
+
+def match_tick_price_buy(price: float, n_tick: int) -> float:
+    """
+    more slip = higher buy price = easier to buy
+    return match_tick_price(price=price, n_tick=n_tick, is_round_up=True)
+    """
+    return match_tick_price(price=price, n_tick=n_tick, is_round_up=True)
+
+
+def match_tick_price_sell(price: float, n_tick: int) -> float:
+    """
+    more slip = lower sell price = easier to sell
+    return match_tick_price(price=price, n_tick=-n_tick, is_round_up=False)
+    """
+    return match_tick_price(price=price, n_tick=-n_tick, is_round_up=False)
