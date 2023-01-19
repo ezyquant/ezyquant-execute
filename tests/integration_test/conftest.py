959b81dec2ba1e404e4ea20d70fe9054374c76ad
Kongtap (Arm) Arunlakvilart <kongtap@fintech.co.th>
2023-01-19 16:38:22 +0700

    test: refactor app id secret

--- tests/integration_test/conftest.py
+++ tests/integration_test/conftest.py
@@ -2,17 +2,22 @@ import pytest
 from settrade_v2.realtime import RealtimeDataConnection
 from settrade_v2.user import Investor
 
-E_INV_ACCOUNT_NO = "8300116"
+APP_CODE = "ALGO_EQ"
+BROKER_ID = "025"
 PIN = "111111"
+
+INV_APP_ID = "CfVAuVWUwcP1grkG"
+INV_APP_SECRET = "AOGH4Zavk0basf6tliHvf1kJuzECnpyoRRiMGpcVEX3O"
+E_INV_ACCOUNT_NO = "8300116"
 
 
 @pytest.fixture
 def stt_inv() -> Investor:
     return Investor(
-        app_id="CfVAuVWUwcP1grkG",
-        app_secret="AOGH4Zavk0basf6tliHvf1kJuzECnpyoRRiMGpcVEX3O",
-        app_code="ALGO_EQ",
-        broker_id="025",
+        app_id=INV_APP_ID,
+        app_secret=INV_APP_SECRET,
+        app_code=APP_CODE,
+        broker_id=BROKER_ID,
     )
 
 
