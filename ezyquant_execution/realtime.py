2215fcff0901dda9a2c54729bfb475c33520c71e
Kongtap Arunlakvilart <kongtap@fintech.co.th>
2023-03-13 10:54:43 +0700

    Refactor/es 147 spread hunter Subscriber exception

--- ezyquant_execution/realtime.py
+++ ezyquant_execution/realtime.py
@@ -1,33 +1,40 @@
 from threading import Event
-from typing import Callable
+from typing import Callable, Optional
 
-from settrade_v2.realtime import RealtimeDataConnection
+from settrade_v2.realtime import RealtimeDataConnection, Subscriber
 
 from .entity import BidOffer, PriceInfo
 
 
 class SettradeSubscriber:
-    def __init__(self, function: Callable, *args, **kwargs):
+    def __init__(self, function: Callable[..., Subscriber], *args, **kwargs):
         self.function = function
         self.args = args
         self.kwargs = kwargs
 
-        self._data: dict = {}
+        self._data: Optional[dict] = {}
+        self._error: Optional[Exception] = None
 
         self._event: Event = Event()
         self.function(on_message=self._on_message, *self.args, **self.kwargs).start()
-        self._event.wait()  # wait for first update
+        self._event.wait(timeout=30)  # wait for first update
 
     @property
     def data(self) -> dict:
+        if self._error:
+            raise self._error
+        if self._data is None:
+            raise ConnectionError("No data received yet")
         return self._data
 
     def _on_message(self, message):
+        self._event.set()
         if message["is_success"]:
             self._data = message["data"]
-            self._event.set()
+            self._error = None
         else:
-            raise ConnectionError(message["message"])
+            self._error = ConnectionError(message["message"])
+            raise self._error
 
 
 class BidOfferSubscriber(SettradeSubscriber):
