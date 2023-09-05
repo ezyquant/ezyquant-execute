4cf09a27a293930572a577d45cd0a1e1212e8b18
Puwa Termnuphan <steampro35200@gmail.com>
2023-09-05 16:03:18 +0700

    added: place order, best bid and ask

--- tests/integration_test/test_derivative_context.py
+++ tests/integration_test/test_derivative_context.py
@@ -1,7 +1,10 @@
 import pytest
 from settrade_v2.user import MarketRep
 
-from ezyquant_execution.derivative_context import ExecuteDerivativeContext
+from ezyquant_execution.derivative_context import (
+    ExecuteDerivativeContext,
+    ExecuteDerivativeContextSymbol,
+)
 
 from .conftest import D_MKT_ACCOUNT_NO
 
@@ -23,4 +26,55 @@ class TestDerivativeExecuteContext:
 
     def test_successs_get_trades(self, exe_ctx: ExecuteDerivativeContext):
         actual = exe_ctx.get_trades()
+        print(actual)
+
+    def test_cancel_orders(self, exe_ctx: ExecuteDerivativeContext):
+        actual = exe_ctx.cancel_orders()
+        print(actual)
+
+    def test_cancel_long_orders(self, exe_ctx: ExecuteDerivativeContext):
+        actual = exe_ctx.cancel_long_orders()
+        print(actual)
+
+    def test_cancel_short_orders(self, exe_ctx: ExecuteDerivativeContext):
+        actual = exe_ctx.cancel_short_orders()
+        print(actual)
+
+
+@pytest.fixture
+def exe_ctx_symbol(stt_mkt: MarketRep):
+    return ExecuteDerivativeContextSymbol(
+        settrade_user=stt_mkt, account_no=D_MKT_ACCOUNT_NO, symbol="S50U23"
+    )
+
+
+class TestDerivativeExecuteContextSymbol:
+    def test_market_price(self, exe_ctx_symbol: ExecuteDerivativeContextSymbol):
+        actual = exe_ctx_symbol.market_price
+        print(actual)
+
+    def test_best_bid_price(self, exe_ctx_symbol: ExecuteDerivativeContextSymbol):
+        actual = exe_ctx_symbol.best_bid_price
+        print(actual)
+
+    def test_best_ask_price(self, exe_ctx_symbol: ExecuteDerivativeContextSymbol):
+        actual = exe_ctx_symbol.best_ask_price
+        print(actual)
+
+    def test_place_order(self, exe_ctx_symbol: ExecuteDerivativeContextSymbol):
+        actual = exe_ctx_symbol.place_order(
+            side="Long",
+            position="Open",
+            price_type="Limit",
+            price=930,
+            volume=1,
+            iceberg_vol=0,
+            validity_type="Day",
+            validity_date_condition=None,
+            stop_condition=None,
+            stop_symbol=None,
+            stop_price=None,
+            trigger_session=None,
+            bypass_warning=True,
+        )
         print(actual)
