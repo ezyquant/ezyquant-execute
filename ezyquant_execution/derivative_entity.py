7be63f910fc54d35d7571587bcd13c554864c8de
Puwa Termnuphan <45455232+propuwaa@users.noreply.github.com>
2023-09-12 09:45:03 +0700

    feat/qd-163-add-derivative-execution (#26)
    * added: place order, best bid and ask
    
    * added: property execute context symbol

--- ezyquant_execution/derivative_entity.py
+++ ezyquant_execution/derivative_entity.py
@@ -1,14 +1,59 @@
 import inspect
 from dataclasses import dataclass
-from typing import List, Literal
+from typing import Any, Dict, List, Literal
+
+import pandas as pd
 
 from . import utils
 
+# ORDER AND TRADE
+SIDE_LONG = "Long"
+SIDE_SHORT = "Short"
 SIDE_TYPE = Literal["Long", "Short"]
+
+# TRADE
 OPEN_CLOSE_TYPE = Literal["AUTO", "OPEN", "CLOSE"]
 STATUS_TYPE = Literal["UNREGISTRATION", "ACTIVE", "RECTIFIED"]
 TRADE_TYPE = Literal["NEW", "REVERSING", "OVERTAKING"]
 CURRENCY_TYPE = Literal["THB", "USD"]
+
+# ORDER REQUEST
+OPEN_POSITION = "Open"
+CLOSE_POSITION = "Close"
+POSITION = Literal["Auto", "Open", "Close"]
+PRICE_TYPE = Literal["Limit", "ATO", "MP-MKT", "MP-MTL"]
+VALIDITY_TYPE = Literal["Day", "FOK", "IOC", "Date", "Cancel"]
+TRIGGER_CONDITION = Literal[
+    "ASK_OR_HIGHER",
+    "ASK_OR_LOWER",
+    "BID_OR_HIGHER",
+    "BID_OR_LOWER",
+    "LAST_PAID_OR_HIGHER",
+    "LAST_PAID_OR_LOWER",
+    "SESSION",
+]
+TRIGGER_SESSION = Literal[
+    "Pre-Open1", "Open1", "Day", "Pre-Open2", "Open2", "Pre-Open0", "Open0"
+]
+
+# QUOTE SYMBOL
+MARKET_STATUS_DISPLAY_TYPE = Literal[
+    "Close",
+    "Pre-Open1",
+    "Pre-Open",
+    "Freeze1",
+    "Open1",
+    "Open",
+    "Intermission1",
+    "Pre-Open2",
+    "Freeze2",
+    "Open2",
+    "Pre-Close",
+    "Freeze3",
+    "OffHour",
+    "Circuit Breaker",
+    "Full Halt",
+]
 
 
 class SettradeStruct:
@@ -22,6 +67,87 @@ class SettradeStruct:
                 if k in inspect.signature(cls).parameters
             }
         )
+
+
+@dataclass
+class StockQuoteResponse(SettradeStruct):
+    instrumentType: str
+    symbol: str
+    high: float
+    low: float
+    last: float
+    average: float
+    change: float
+    percentChange: float
+    totalVolume: int
+    marketName: str
+    marketStatus: MARKET_STATUS_DISPLAY_TYPE
+    underlying: str
+    underlyingPrice: float
+    multiplier: int
+    expDate: str
+    lastTradingDate: str
+    spread: float
+    settlement: float
+    previousSettle: float
+    openInterest: int
+    theoretical: float
+    basis: float
+
+
+# Market Section
+@dataclass
+class BidOfferItem:
+    price: float
+    volume: int
+
+
+@dataclass
+class BidOffer:
+    symbol: str
+    bids: List[BidOfferItem]
+    asks: List[BidOfferItem]
+
+    @property
+    def best_bid_price(self):
+        return self.bids[0].price
+
+    @property
+    def best_bid_volume(self):
+        return self.bids[0].volume
+
+    @property
+    def best_ask_price(self):
+        return self.asks[0].price
+
+    @property
+    def best_ask_volume(self):
+        return self.asks[0].volume
+
+    @property
+    def dataframe(self):
+        data = {
+            "bid_volume": [i.volume for i in self.bids],
+            "bid_price": [i.price for i in self.bids],
+            "ask_price": [i.price for i in self.asks],
+            "ask_volume": [i.volume for i in self.asks],
+        }
+        return pd.DataFrame(data)
+
+    def __str__(self):
+        return self.dataframe.to_string()
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        bids = [
+            BidOfferItem(price=data[f"bid_price{i}"], volume=data[f"bid_volume{i}"])
+            for i in range(1, 11)
+        ]
+        asks = [
+            BidOfferItem(price=data[f"ask_price{i}"], volume=data[f"ask_volume{i}"])
+            for i in range(1, 11)
+        ]
+        return cls(data["symbol"], bids, asks)
 
 
 @dataclass
@@ -248,6 +374,114 @@ class DerivativeTotalPortfolio(SettradeStruct):
 
 
 @dataclass
+class DerivativeOrder(SettradeStruct):
+    order_no: int
+    """Order number"""
+    tfx_order_no: str
+    """TFEX order number (Pattern: orderbook-side-orderid)"""
+    account_no: str
+    """Account number"""
+    entry_id: str
+    """Entry Id (If the order placed by marketing representative)"""
+    entry_time: str
+    """Time of transaction sent to Settrade (yyyy-MM-dd'T'HH:mm:ss)"""
+    trade_date: str
+    """Trade date (formatted as yyyy-MM-dd)"""
+    transaction_time: str
+    """Time of transaction sent to TFEX (yyyy-MM-dd'T'HH:mm:ss)"""
+    cancel_id: str  # Nullable field
+    """Cancel Id (If the order canceled by marketing representative)"""
+    cancel_time: str  # Nullable field
+    """Cancel time (yyyy-MM-dd'T'HH:mm:ss)"""
+    symbol: str
+    """Symbol"""
+    side: SIDE_TYPE
+    """Order side:
+    * `Long` - Buy
+    * `Short` - Sell"""
+    position: POSITION
+    """Order position:
+    * `Open` - Open Position
+    * `Close` - Close Position
+    * `Auto` - Auto Position (extra permission required)"""
+    price_type: PRICE_TYPE
+    """Price type:
+    * `Limit` - Limit Order
+    * `ATO` - At The Open (field price must be 0)
+    * `MP-MTL` - Market To Limit Order (field price must be 0)
+    * `MP-MKT` - Market Order (field price must be 0)"""
+    price: float
+    """Price"""
+    qty: int
+    """Volume (Balance volume + Matched volume + Cancelled volume)"""
+    iceberg_vol: int
+    """Iceberg Volume"""
+    balance_qty: int
+    """Balance volume"""
+    match_qty: int
+    """Matched volume"""
+    cancel_qty: int
+    """Cancelled volume"""
+    validity: VALIDITY_TYPE
+    """Order validity:
+    * `Day` - Order will be available only on the order entry date
+    * `FOK` - Fill or Kill
+    * `IOC` - Immediate or Cancel
+    * `Date` - GTD order will be available to a specific date
+    * `Cancel` - GTC order will be available for a maximum of 254 days after the business date"""
+    valid_to_date: str  # Nullable field
+    """Valid to date (Format: yyyy-MM-dd) for validity type Date or Cancel (after TFEX accepts the order)"""
+    is_stop_order_not_activate: str
+    """Flag indicating if the stop order is waiting for activation"""
+    trigger_condition: TRIGGER_CONDITION  # Nullable field
+    """Trigger condition for stop order:
+    * `ASK_OR_HIGHER` - Stop price >= Ask price (Condition type: Price Movement)
+    * `ASK_OR_LOWER` - Stop price <= Ask price (Condition type: Price Movement)
+    * `BID_OR_HIGHER` - Stop price >= Bid price (Condition type: Price Movement)
+    * `BID_OR_LOWER` - Stop price <= Bid price (Condition type: Price Movement)
+    * `LAST_PAID_OR_HIGHER` - Stop price >= Last price (Condition type: Price Movement)
+    * `LAST_PAID_OR_LOWER` - Stop price <= Last price (Condition type: Price Movement)
+    * `SESSION` - Stop order will trigger when the trading session changes"""
+    trigger_symbol: str  # Nullable field
+    """Trigger symbol for stop order"""
+    trigger_price: float
+    """Trigger price for stop order"""
+    trigger_session: TRIGGER_SESSION  # Nullable field
+    """Trigger session (required if triggerCondition = SESSION): 
+    * 'Pre-Open1' - Pre Open before Morning Session and Day Session
+    * 'Open1' - Morning Session
+    * 'Day' - Day Session (for non-intermission product e.g. RSS, RSS3D)
+    * 'Pre-Open2' - Pre Open before Afternoon Session
+    * 'Open2' - Afternoon Session
+    * 'Pre-Open0' - Pre Open before Night Session (for night session product e.g. GOLD, CURRENCY)
+    * 'Open0' - Night Session (for night session product e.g. GOLD, CURRENCY)"""
+    status: str
+    """Order status (Abbreviation)"""
+    show_status: str
+    """Order status (For display)"""
+    status_meaning: str
+    """Order status meaning"""
+    reject_code: int
+    """Reject code"""
+    reject_reason: str  # Nullable field
+    """Description of rejectCode"""
+    cpm: str  # Nullable field
+    """Counter party member"""
+    tr_type: str  # Nullable field
+    """Trade report type"""
+    terminal_type: str  # Nullable field
+    """Terminal type"""
+    version: int
+    """Version of order status"""
+    can_cancel: bool
+    """Flag indicating whether the order is allowed to be cancelled"""
+    can_change: bool
+    """Flag indicating whether the order is allowed to be changed"""
+    price_digit: int
+    """Decimal point of Price value"""
+
+
+@dataclass
 class DerivativeTrade(SettradeStruct):
     broker_id: str
     """Broker Id"""
@@ -312,3 +546,14 @@ class DerivativeTrade(SettradeStruct):
     """Reject code"""
     reject_reason: str
     """Reject reason"""
+
+
+@dataclass
+class CancelOrder(SettradeStruct):
+    order_no: str
+    """Order number"""
+    error_response: Dict[str, Any]
+    http_status: str
+    """HTTP status"""
+    http_status_code: int
+    """HTTP status code"""
